f2c13c0d0227f7b1c63aa58339c2d7334ae5e1f6
luctia <timmerman.luc1999@gmail.com>
2020-12-02 13:44:33 +0100

    Finished day 2

---
+++ day2.hs
@@ -0,0 +1,27 @@
+module Main where
+
+import Day2data
+import Data.List
+import Data.Bool
+
+main = do
+    print (part1)
+    print (part2)
+
+part1 :: Int
+part1 = length [ x | x <- getdata, isValid1 x ]
+
+isValid1 :: (Int, Int, Char, String) -> Bool
+isValid1 (start, end, char, input) = occ >= start && occ <= end where occ = countElem char input
+
+part2 :: Int
+part2 = length [ x | x <- getdata, isValid2 x ]
+
+isValid2 :: (Int, Int, Char, String) -> Bool
+isValid2 (first, second, char, input) = xor ((input!!(first - 1)) == char) ((input!!(second - 1)) == char)
+
+countElem :: (Eq a) => a -> [a] -> Int
+countElem needle haystack = length [ x | x <- haystack, x == needle ]
+
+xor :: Bool -> Bool -> Bool
+xor stat1 stat2 = (stat1 /= stat2) && (stat1 || stat2)
