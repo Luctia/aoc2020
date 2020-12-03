2012e065f6d8d88233057b466c697028de3af400
luctia <timmerman.luc1999@gmail.com>
2020-12-03 13:53:58 +0100

    Part 2

--- day3.hs
+++ day3.hs
@@ -5,3 +5,11 @@ gaanMetDieBanaan right down col line inputlines
   | otherwise = gaanMetDieBanaan right down ((col + right) `mod` 31) (line + down) inputlines
 
 part1 = (gaanMetDieBanaan 3 1 0 0 . lines) <$> readFile "day3input.txt"
+
+steps :: [(Int, Int)]
+steps = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+
+getAll :: [String] -> [Int]
+getAll input = [ res | (right, down) <- steps, let res = gaanMetDieBanaan right down 0 0 input ]
+
+part2 = (foldl (*) 1 . getAll . lines) <$> readFile "day3input.txt"
