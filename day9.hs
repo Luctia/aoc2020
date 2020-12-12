138aae14caa5c6a7faeadcb68fc598b4f637fb40
luctia <timmerman.luc1999@gmail.com>
2020-12-12 19:16:35 +0100

    Day 9 part 2

--- day9.hs
+++ day9.hs
@@ -1,9 +1,11 @@
+import Data.List
+
 numberIsValid :: [Int] -> Int -> Bool
 numberIsValid list index
-  | index < 25 = True
+  | index < 5 = True
   | elem (list !! index) (allAditions subset) = True
   | otherwise = False
-  where subset = take 25 . drop (index - 25) $ list
+  where subset = take 5 . drop (index - 5) $ list
 
 allAditions :: [Int] -> [Int]
 allAditions list = [ x + y | x <- list, y <- list]
@@ -13,4 +15,20 @@ getFirstMistake index list
   | numberIsValid list index = getFirstMistake (index + 1) list
   | otherwise = list !! index
 
-part1 = (getFirstMistake 25 . map (\x -> read x::Int) . lines) <$> readFile "day9input.txt"
+part1 = (getFirstMistake 5 . map (\x -> read x::Int) . lines) <$> readFile "day9input.txt"
+
+incorrectNumber = 1492208709
+
+findCombo :: Int -> [Int] -> [Int]
+findCombo outcome list = head $ filter ((==outcome) . sum) (allContiguousSubsets 0 list)
+
+findWeakness :: [Int] -> Int
+findWeakness combo = (head (sort combo)) + (last (sort combo))
+
+allContiguousSubsets :: Int -> [a] -> [[a]]
+allContiguousSubsets _ [] = [[]]
+allContiguousSubsets start list
+  | start == length list - 1 = []
+  | otherwise = [ drop start . take x $ list | x <- [1..(length list)] ] ++ allContiguousSubsets (start + 1) list
+
+part2 = (findWeakness . findCombo incorrectNumber . map (\x -> read x::Int) . lines) <$> readFile "day9input.txt"
