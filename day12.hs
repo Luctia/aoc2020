882b6afbc43a3e86dbf34131599f1683d7a66cde
luctia <timmerman.luc1999@gmail.com>
2020-12-13 19:37:52 +0100

    Day 12 part 2

--- day12.hs
+++ day12.hs
@@ -53,3 +53,43 @@ calculateManhattanDistance :: State -> Int
 calculateManhattanDistance (State (x, y) _) = abs x + abs y
 
 part1 = (calculateManhattanDistance . executeInstructions (State (0, 0) East) . map stringToInstructions . lines) <$> readFile "day12input.txt"
+
+data WPInst = WPMove Direction Int
+            | WPTurn Char Int
+            | WPForward Int
+            deriving (Show)
+
+data WPState = WPState (Int, Int) (Int, Int) deriving (Show)
+
+stringToWPInst :: String -> WPInst
+stringToWPInst (c:v)
+  | c == 'F' = WPForward value
+  | c == 'L' || c == 'R' = WPTurn c value
+  | otherwise = WPMove (charToDirection c) value
+  where value = read v::Int
+
+rotate :: (Int, Int) -> Int -> (Int, Int)
+rotate pair 0 = pair
+rotate (x, y) i = rotate (y, -x) (i - 1)
+
+executeWpInst :: WPState -> WPInst -> WPState
+executeWpInst (WPState (bx, by) (wx, wy)) (WPMove direction steps)
+  | direction == North = WPState (bx, by) (wx, wy + steps)
+  | direction == East = WPState (bx, by) (wx + steps, wy)
+  | direction == South = WPState (bx, by) (wx, wy - steps)
+  | otherwise = WPState (bx, by) (wx - steps, wy)
+executeWpInst (WPState (bx, by) (wx, wy)) (WPTurn direction degrees)
+  | direction == 'R' = WPState (bx, by) (rotate (wx, wy) noSteps)
+  | otherwise = WPState (bx, by) (rotate (wx, wy) noStepsCC)
+  where noSteps = mod (div degrees 90) 4
+        noStepsCC = if mod noSteps 2 == 0 then noSteps else noSteps + 2
+executeWpInst (WPState (bx, by) (wx, wy)) (WPForward steps) = WPState (bx + wx * steps, by + wy * steps) (wx, wy)
+
+executeWpInsts :: WPState -> [WPInst] -> WPState
+executeWpInsts state [] = state
+executeWpInsts state (inst:insts) = executeWpInsts (executeWpInst state inst) insts
+
+calculateManhattanDistance2 :: WPState -> Int
+calculateManhattanDistance2 (WPState (x, y) _) = abs x + abs y
+
+part2 = (calculateManhattanDistance2 . executeWpInsts (WPState (0, 0) (10, 1)) . map stringToWPInst . lines) <$> readFile "day12input.txt"
