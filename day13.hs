66e78caf87ec02434a1452325b3e0b4b4502d9b4
luctia <timmerman.luc1999@gmail.com>
2020-12-13 14:28:49 +0100

    Day 13 part 2

--- day13.hs
+++ day13.hs
@@ -1,4 +1,5 @@
 import Data.List.Split
+import Data.List
 
 getTimestamps :: [String] -> [Int]
 getTimestamps [] = []
@@ -20,3 +21,24 @@ part1' [[start], is] = getResult (getBestBus startInt (getTimestamps is) 10000) 
                      where startInt = (read start::Int)
 
 part1 = (part1' . map (splitOn ",") . lines) <$> readFile "day13data.txt"
+
+-- For part 2, we use the Chinese remainder theorem.
+
+getIdIndexPairs :: Int -> [String] -> [(Int, Int)]
+getIdIndexPairs index xs
+  | index > length xs - 1 = []
+  | xs !! index == "x" = getIdIndexPairs (index + 1) xs
+  | otherwise = (index, (read (xs !! index)::Int)):(getIdIndexPairs (index + 1) xs)
+
+getSolution :: [(Int, Int)] -> Int
+getSolution pairs = total `mod` bign
+                  where bign = foldl (*) 1 (map snd pairs)
+                        total = sum [ b * n * x | pair <- pairs,
+                                                  let b = (snd pair) - (fst pair),
+                                                  let n = div bign (snd pair),
+                                                  let x = calculateX n (snd pair) ]
+
+calculateX :: Int -> Int -> Int
+calculateX multiplier modulator = head [ x | x <- [1..multiplier], mod (x * multiplier) modulator == 1 ]
+
+part2 = (getSolution . getIdIndexPairs 0 . splitOn "," . (!! 1) .lines) <$> readFile "day13data.txt"
