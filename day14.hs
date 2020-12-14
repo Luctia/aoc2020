5aadd9d9b43c1d61473b483426b5b1961a366ab9
luctia <timmerman.luc1999@gmail.com>
2020-12-14 13:14:35 +0100

    Day 14 part 2

--- day14.hs
+++ day14.hs
@@ -46,3 +46,30 @@ getMemorySum :: [(Int, Int)] -> Int
 getMemorySum memory = sum $ map snd memory
 
 part1 = (getMemorySum . walkThroughInput [] "" . lines) <$> readFile "day14input.txt"
+
+allPosibilities :: [Int] -> String -> [[Int]] -> [[Int]]
+allPosibilities [] [] completed = completed
+allPosibilities (i:input) (m:mask) completed
+  | m == '0' = allPosibilities input mask (map (++[i]) completed)
+  | m == '1' = allPosibilities input mask (map (++[(read [m]::Int)]) completed)
+  | m == 'X' = allPosibilities input mask ((map (++[1]) completed) ++ (map (++[0]) completed))
+allPosibilities _ _ _ = error "Bitmask does not have the same length as input value"
+
+walkThroughInput2 :: [(Int, Int)] -> String -> [String] -> [(Int, Int)]
+walkThroughInput2 memory _ [] = memory
+walkThroughInput2 memory mask (inst:insts)
+  | take 3 inst == "mas" = walkThroughInput2 memory (drop 7 inst) insts
+  | otherwise = walkThroughInput2 (addToMemory ++ memory) mask insts
+  where splitted = splitOn " = " inst
+        newValue = read (splitted !! 1)::Int
+        location = read (init (drop 4 (splitted !! 0)))::Int
+        allpos = map (binToDec) (allPosibilities (decToBin location) mask [[]])
+        addToMemory = [ (pos, newValue) | pos <- allpos ]
+
+getMemorySum2 :: [Int] -> [(Int, Int)] -> Int
+getMemorySum2 _ [] = 0
+getMemorySum2 seen (m:memory)
+  | elem (fst m) seen = getMemorySum2 seen memory
+  | otherwise = (snd m) + getMemorySum2 ((fst m):seen) memory
+
+part2 = (getMemorySum2 [] . walkThroughInput2 [] "" . lines) <$> readFile "day14input.txt"
